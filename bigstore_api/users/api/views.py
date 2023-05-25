540a1daf3ddb7925d366e374fb304adfb92187ed
Leonardo Lima <se.leonardolima@gmail.com>
2023-05-25 11:29:24 -0300

    Modify userviewset, to use modelviewset and add permissions in post route
    Modify userviewset, to use modelviewset and allow users to access multiple routes and be created without being authenticated

--- bigstore_api/users/api/views.py
+++ bigstore_api/users/api/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from .serializers import UserSerializer
 
 User = get_user_model()
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = "pk"
@@ -18,6 +18,11 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def get_queryset(self, *args, **kwargs):
         assert isinstance(self.request.user.id, int)
         return self.queryset.filter(id=self.request.user.id)
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        return super().get_permissions()
 
     @action(detail=False)
     def me(self, request):
