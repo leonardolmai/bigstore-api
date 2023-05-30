f8fb9f7f29b38cf7cf3afd97a456693eee5e4c0f
Leonardo Lima <se.leonardolima@gmail.com>
2023-05-30 09:40:49 -0300

    Add company relationship to user at login time if not exist

--- bigstore_api/users/backends.py
+++ bigstore_api/users/backends.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import serializers
+
+from bigstore_api.users.models import Company, UserCompany
 
 User = get_user_model()
 
@@ -8,6 +11,15 @@ User = get_user_model()
 class UserActivationBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         user = super().authenticate(request, username=username, password=password, **kwargs)
+
+        company_cnpj = request.headers.get("x-company-cnpj")
+
+        if company_cnpj:
+            try:
+                company = Company.objects.get(cnpj=company_cnpj)
+                UserCompany.objects.get_or_create(user=user, company=company)
+            except Company.DoesNotExist:
+                raise serializers.ValidationError("Invalid company CNPJ")
 
         if user is not None and not user.is_active:
             user.is_active = True
