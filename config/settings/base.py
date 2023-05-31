7068cfd4b936e733083c7a8e90f4c33061d9478b
Leonardo Lima <se.leonardolima@gmail.com>
2023-05-31 17:21:53 -0300

    Start products app

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,6 +84,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "bigstore_api.users",
     # Your stuff: custom apps go here
+    "bigstore_api.products",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
