e932b1626f24fdb89d670551f18c055620bebfdb
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-06-01 12:02:37 -0300

    Created route address in infouser

--- bigstore_api/infouser/models.py
+++ bigstore_api/infouser/models.py
@@ -20,3 +20,11 @@ class CardUser(Model):
     user_fk = ForeignKey(settings.AUTH_USER_MODEL,on_delete=CASCADE,related_name="Fk_User")
     card_fk = ForeignKey(Card, on_delete=CASCADE,related_name="Fk_Card")
 
+
+class Address(Model):
+    id_user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+    cep = CharField("CEP", max_length=8, blank=False)  # 8 caracteres cep
+    uf = CharField("UF", max_length=255, blank=False)
+    city = CharField(_("City"), max_length=255, blank=False)
+    address1 = CharField(_("Address 1"), max_length=255, blank=False)
+    address2 = CharField(_("Address 2"), max_length=255, blank=True)
