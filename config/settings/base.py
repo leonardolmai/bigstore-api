ee91589b7403f827130a52414a6b3061e327d0b4
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-06-04 17:23:59 -0300

    update endpoint router-addresses
    update local-app addresses

--- config/settings/base.py
+++ config/settings/base.py
@@ -88,6 +88,7 @@ LOCAL_APPS = [
     "bigstore_api.users",
     # Your stuff: custom apps go here
     "bigstore_api.products",
+    "bigstore_api.addresses",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
