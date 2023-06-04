aa28e23f55a9ecd43b43ada9b114179e0f3a0e53
Leonardo Lima <se.leonardolima@gmail.com>
2023-06-03 21:30:09 -0300

    Create product and productimage serializers

---
+++ bigstore_api/products/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from bigstore_api.products.models import Product, ProductImage
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ["id", "image"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, required=False)
+
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "price",
+            "description",
+            "is_approved",
+            "created_by",
+            "company",
+            "category",
+            "images",
+        ]
+        read_only_fields = ["id", "created_by", "company", "images"]
