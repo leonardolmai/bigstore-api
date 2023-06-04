f13200779f05b97d14f4f43e6dae6e85c61af55d
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-06-04 16:59:10 -0300

    Create models addresses

--- bigstore_api/addresses/models.py
+++ bigstore_api/addresses/models.py
@@ -1,3 +1,14 @@
-# from django.db import models
+from django.conf import settings
+from django.db.models import CASCADE, CharField, ForeignKey, Model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Address(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="addresses")
+    postal_code = CharField(_("postal code"), max_length=8)
+    uf = CharField("UF", max_length=150, blank=False)
+    city = CharField(_("City"), max_length=150, blank=False)
+    neighborhood = CharField(_("neighborhood"), max_length=150)
+    street = CharField(_("street"), max_length=150)
+    number = CharField(_("number"), max_length=20)
+    complement = CharField(_("complement"), max_length=150, blank=True, default="")
