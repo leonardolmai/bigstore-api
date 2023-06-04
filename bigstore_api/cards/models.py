65a16503ff225880f6b28bae1de083d7daca20a9
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-06-04 17:59:31 -0300

    Create models cards

--- bigstore_api/cards/models.py
+++ bigstore_api/cards/models.py
@@ -1,3 +1,12 @@
-# from django.db import models
+from django.conf import settings
+from django.db.models import CASCADE, CharField, ForeignKey, Model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Card(Model):
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="user")
+    name = CharField(_("Cardholder"), max_length=255, blank=False)
+    number = CharField(_("Card Number"), max_length=16, blank=False)
+    expiration_month = CharField(_("expiration month"), max_length=2)
+    expiration_year = CharField(_("expiration Year"), max_length=4)
+    cvc = CharField("CVC", max_length=4, blank=False)
