fd8d9e9cca195ce8c144e9d42d0cfe005ad1f576
Leonardo Lima <se.leonardolima@gmail.com>
2023-06-04 21:38:51 -0300

    Add read-only fields and set user field to not required in address serializer

--- bigstore_api/addresses/api/serializers.py
+++ bigstore_api/addresses/api/serializers.py
@@ -5,8 +5,9 @@ from bigstore_api.users.api.serializers import UserSerializer
 
 
 class AddressSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer(required=False)
 
     class Meta:
         model = Address
-        fields = ("id", "user", "postal_code", "uf", "city", "neighborhood", "street", "number", "complement")
+        fields = ["id", "user", "postal_code", "uf", "city", "neighborhood", "street", "number", "complement"]
+        read_only_fields = ["id", "user"]
