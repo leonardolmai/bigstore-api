3371baf3cd787c7065a3b1c5fe909f1436076a72
Leonardo Lima <se.leonardolima@gmail.com>
2023-06-04 21:35:44 -0300

    Change the maximum length of the uf

--- bigstore_api/addresses/models.py
+++ bigstore_api/addresses/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 class Address(Model):
     user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="addresses")
     postal_code = CharField(_("postal code"), max_length=8)
-    uf = CharField("UF", max_length=150, blank=False)
+    uf = CharField("UF", max_length=2, blank=False)
     city = CharField(_("city"), max_length=150, blank=False)
     neighborhood = CharField(_("neighborhood"), max_length=150)
     street = CharField(_("street"), max_length=150)
