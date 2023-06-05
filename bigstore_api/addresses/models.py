d5269873d4be5ee59fec57ba37cb1e66b9da6a8e
Leonardo Lima <se.leonardolima@gmail.com>
2023-06-04 21:14:41 -0300

    Fix typo

--- bigstore_api/addresses/models.py
+++ bigstore_api/addresses/models.py
@@ -7,7 +7,7 @@ class Address(Model):
     user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="addresses")
     postal_code = CharField(_("postal code"), max_length=8)
     uf = CharField("UF", max_length=150, blank=False)
-    city = CharField(_("City"), max_length=150, blank=False)
+    city = CharField(_("city"), max_length=150, blank=False)
     neighborhood = CharField(_("neighborhood"), max_length=150)
     street = CharField(_("street"), max_length=150)
     number = CharField(_("number"), max_length=20)
