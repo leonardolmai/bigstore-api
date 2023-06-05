3b50f7ef965abef91a654bf1c3762d632eec708c
Leonardo Lima <se.leonardolima@gmail.com>
2023-06-05 07:14:28 -0300

    Fix typo and change user related name on card model

--- bigstore_api/cards/models.py
+++ bigstore_api/cards/models.py
@@ -4,9 +4,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Card(Model):
-    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="user")
+    user = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="cards")
     name = CharField(_("Cardholder"), max_length=255, blank=False)
-    number = CharField(_("Card Number"), max_length=16, blank=False)
+    number = CharField(_("card number"), max_length=16, blank=False)
     expiration_month = CharField(_("expiration month"), max_length=2)
-    expiration_year = CharField(_("expiration Year"), max_length=4)
+    expiration_year = CharField(_("expiration year"), max_length=4)
     cvc = CharField("CVC", max_length=4, blank=False)
