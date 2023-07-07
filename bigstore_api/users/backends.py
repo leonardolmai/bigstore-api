501a7db1bc2f9db9874081b8ea0fa2aeb9f9f9a1
Leonardo Lima <se.leonardolima@gmail.com>
2023-07-07 08:28:21 -0300

    Generate an error if credentials are not correct

--- bigstore_api/users/backends.py
+++ bigstore_api/users/backends.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
 
 from bigstore_api.users.models import Company, UserCompany
 
@@ -11,6 +12,9 @@ User = get_user_model()
 class UserActivationBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         user = super().authenticate(request, username=username, password=password, **kwargs)
+
+        if user is None:
+            raise AuthenticationFailed("Unable to log in with the provided credentials")
 
         company_cnpj = request.headers.get("x-company-cnpj")
 
